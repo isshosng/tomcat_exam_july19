b3363df3f64d19d53467cb773115e3a9bb0a911d
isshosng <purehug@hanmail.net>
2022-07-21 18:18:46 +0900

    jsp

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -12,12 +12,24 @@ public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 들어오는 파라미터를 UTF-8로 해석
-        req.setCharacterEncoding("UTF-8");
+        /*req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html; charset=utf-8");// HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
+        resp.setContentType("text/html; charset=utf-8");// HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.*/
+
+        Rq rq = new Rq(req, resp);
+
+        int dan = rq.getIntParam("dan",9);
+        int limit = rq.getIntParam("limit",9);
+
+        rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
+
+        for (int i=1; i<=limit; i++){
+            rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan*i));
+        }
 
 
-        int dan = Integer.parseInt(req.getParameter("dan"));
+
+        /*int dan = Integer.parseInt(req.getParameter("dan"));
         int limit = Integer.parseInt(req.getParameter("limit"));
 
 
@@ -25,6 +37,6 @@ public class GugudanServlet extends HttpServlet {
 
         for (int i=1; i<=limit; i++){
             resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan*i));
-        }
+        }*/
     }
 }
